508307728a67ee6e5eb7a9cce52ab3256b3c3bd0
jrwetmore1 <135853570+jrwetmore1@users.noreply.github.com>
2023-06-08 16:01:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    // The linear line Equation is: Z = 4X2+3Y, when  X = 2.5, Y=3.3, write a program to:
+
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z = 4*x*x+3*y;
+        Console.WriteLine(z);
+    }
+}
