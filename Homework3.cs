f28dd5199451d384140aee57e3a7e6e77e5c3794
jrwetmore1 <135853570+jrwetmore1@users.noreply.github.com>
2023-06-26 22:19:00 -0700

    Update Homework3.cs

--- Homework3.cs
+++ Homework3.cs
@@ -17,7 +17,7 @@ class Program
             Console.WriteLine($"{number} is non-prime.");
     }
 
-    //checks if the number inputed is a prime or not
+    //checks if the number inputed is prime or not
     static bool IsPrime(int number)
     {
         if (number < 2)
@@ -35,3 +35,56 @@ class Program
         return true;
     }
 }
+
+
+
+
+namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //ask the user to assign a number 
+    Console.WriteLine("Assign an int value to N: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        for(int i = 0;i<n;i++){
+            
+        for(int j =0;j<n;j++){
+
+            //after user input, the console writes out how many # are needed
+            Console.Write("#");
+        }
+            Console.WriteLine();
+        }
+  }
+}
+
+
+
+
+namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+int i,j,n;
+Console.Write("Assign an int value to N: ");
+n= int.Parse(Console.ReadLine());
+
+
+for(i=1; i<=n; i++)
+
+
+{
+for(j=1; j<=i; j++)
+{
+Console.Write("*");
+}
+Console.Write("\n");
+}
+}
+}
+
