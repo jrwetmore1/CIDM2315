dd2c2e23747f9850d5c837cf8dcdc4f51a55cfc4
jrwetmore1 <135853570+jrwetmore1@users.noreply.github.com>
2023-06-26 23:49:11 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ask user to enter a number
+        Console.Write("Input an integer:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPrime = IsPrime(number);
+
+        //message back to the user after a number is entered
+        if (isPrime)
+            Console.WriteLine($"{number} is prime.");
+        else
+            Console.WriteLine($"{number} is non-prime.");
+    }
+
+    //checks if the number inputed is a prime or not
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+                break;
+            }
+        }
+
+        return true;
+    }
+}
