9741d52bc2f906002f2e70fbea008fd079565772
jrwetmore1 <135853570+jrwetmore1@users.noreply.github.com>
2023-07-03 23:36:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework4;
+
+using System;
+
+public class Largest
+{
+    public static int largest(int n1, int n2)
+    {
+
+        //check if the first integer is larger
+        if (n1 > n2)
+        {
+            return n1;
+        }
+        else
+        {
+            return n2;
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+
+        //ask user to enter integer
+        Console.Write("Enter first integer: ");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second integer: ");
+        int second = Convert.ToInt32(Console.ReadLine());
+
+        //prints which integer is the largest
+        Console.WriteLine("The largest number is: " + largest(first, second));
+    }
+}
